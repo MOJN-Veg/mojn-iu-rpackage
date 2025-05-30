2b532a42ba5423a1731dbba8da0e8a397a1a8ea1
Sarah Wright <sarah_wright@nps.gov>
2025-05-30 15:18:02 -0600

    Update demo script

--- temporary_demo_script.R
+++ temporary_demo_script.R
@@ -3,8 +3,7 @@ remotes::install_github("MOJN-Veg/mojn-iu-rpackage")
 keyring::key_set("AGOL", "mojn_data")  # Run this to save password to keyring - only need to do this once per user per computer
 
 # Get the data
-raw_data <- integrateduplands:::fetchRawIU()
-raw_data <- integrateduplands:::wrangleIU(raw_data)
+raw_data <- integrateduplands:::loadAndWrangleIU()
 
 # Look at visit table
 raw_data$data$Visit
